ee94e1d6a89c340eecde1412f2fcd014dd622f80
Lucasrusso36 <lucaspidossantos@gmail.com>
2023-02-11 10:26:06 -0300

    Estruturas de controle

---
+++ onebitcode/exercicios/calculation_2.rb
@@ -0,0 +1,11 @@
+#Utilizando as estruturas de iteração e condição, crie uma calculadora que ofereça ao usuário 
+#a opção de Multiplicar, Dividir, Adicionar ou Subtrair dois números. Não se esqueça de também permitir que o usuário feche o programa. 
+
+print "Digite o primeiro número inteiro: "
+# .to_i transforma a string em um número inteiro
+number1 = gets.chomp.to_i
+
+print "Digite o segundo número inteiro: "
+
+
+number2 = gets.chomp.to_i
